d17bb160af49ba2a9c164a255535a8c38bdb8022
creaboulait <norshida.razali@gmail.com>
2018-07-05 06:34:19 +0800

    Added Google OAuth

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,2 +1,2 @@
 # config/initializers/figaro.rb
-Figaro.require_keys("pusher_app_id", "pusher_key", "pusher_secret")
+# Figaro.require_keys("pusher_app_id", "pusher_key", "pusher_secret")
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  # redirects the user from Google to our app
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
 end
