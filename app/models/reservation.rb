a8b5615d06fc72ef0c7f02c4c842deec9be5070e
creaboulait <norshida.razali@gmail.com>
2018-07-10 22:18:15 +0800

    Included Bootstrap for Reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,12 +2,13 @@ class Reservation < ApplicationRecord
 	belongs_to :user
 	belongs_to :listing
 
-	# validate :overlapping
+	validate :overlapping
 
-	# private
+	private
 	
-	# def overlapping
- #    if Reservation.where('? <  end_date and ? > start_date', self.start_date, self.end_date).any?
- #        errors.add(:end_date, 'Your chosen dates are not available.')
- #    end
+	def overlapping
+    	if Reservation.where('? <  end_date AND ? > start_date', self.start_date, self.end_date).any?
+    	    errors.add(:end_date, 'Your chosen dates are not available.')
+    	end
+	end
 end
