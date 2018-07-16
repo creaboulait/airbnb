61a1788bc367ee10298bf0df2f84a4fede0c1baf
creaboulait <norshida.razali@gmail.com>
2018-07-16 19:57:23 +0800

    Working search and filter w/o AJAX

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,9 +10,11 @@ class Listing < ApplicationRecord
   		update_attribute(:verification, true)
  	end
 
- 	scope :city_search, -> (city_search) { where area: city_search }
- 	scope :bedrooms_search, -> (bedrooms_search) { where bedrooms_num: bedrooms_search }
- 	scope :bathrooms_search, -> (bathrooms_search) { where bathrooms_num: bathrooms_search }
+ 	scope :city_search, -> (city_search) { where "area = ?", city_search }
+ 	scope :bedrooms_search, -> (bedrooms_search) { where "bedrooms_num = ?", bedrooms_search }
+ 	scope :bathrooms_search, -> (bathrooms_search) { where "bathrooms_num = ?", bathrooms_search }
+ 	scope :price_range, -> (min_price, max_price) { where "price > ? AND price < ?", min_price, max_price }
+ 	
 
 
  # 	def self.search(attribute)
@@ -26,6 +28,3 @@ class Listing < ApplicationRecord
 end
 
 
-#   scope :status, -> (status) { where status: status }
-#   scope :location, -> (location_id) { where location_id: location_id }
-#   scope :starts_with, -> (name) { where("name like ?", "#{name}%}
