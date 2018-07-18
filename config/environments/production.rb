c4659e3026112288431075f21d0b93601feadcd4
creaboulait <norshida.razali@gmail.com>
2018-07-18 16:08:09 +0800

    installed rspec gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # Action Mailer Configuration for Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:             ENV['GMAIL_USERNAME'],
+  password:              ENV['GMAIL_PASSWORD'],
+  authentication:       'login', #try login if doesnt work.original is plain
+  enable_starttls_auto: true  }
 end
